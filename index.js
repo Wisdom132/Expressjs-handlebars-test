d69aa409549d275e71f1c6f1b16a6861bb05788e
Wisdom Ekpot <wisdomekpot@gmail.com>
2019-11-23 22:14:15 +0100

    a little test with express-handlebars

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+const app = express();
+
+PORT = 2200;
+
+app.use(express.static("public"));
+
+app.engine(
+  "handlebars",
+  exphbs({
+    defaultLayout: "main",
+    partialsDir: "views/test",
+    extname: "handlebars"
+  })
+);
+app.set("view engine", "handlebars");
+
+// routing
+app.get("/", (req, res) => {
+  res.render("home", {
+    title: "Home",
+    style: "style",
+    message: "Hello wisdom , i am typing from index.js",
+    isShow: false
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    people: ["wisdom", "stbc", "sandra", "cammy"],
+    user: { name: "Wisdom Ekpot", level: "200", age: 21 },
+    style: "about",
+    lists: [
+      { items: ["mango", "flower", "yam", "jam", "bread"] },
+      { items: ["study", "bread", "cookie", "cheese"] }
+    ]
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`App started on port ${PORT}`);
+});
